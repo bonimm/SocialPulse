8b87860c51d9467ed2953db328116a4d6d39c59a
bonimm <bonimonteromunoz@gmail.com>
2026-02-23 13:34:33 +0100

    Configurar Dockerfile con mysqli y conexion PHP-MYSQL exitosa

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,13 @@
 <?php 
-echo "SocialPulse funcionando!"
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error){
+    die("Error de conexión: " . $conn->connect_error);
+} else {
+    echo "¡Conexión a MySQL exitosa!";
+}
 ?>
